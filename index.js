6ce97a059173ec06acfd3ae306ec41e4ecc323bc
Lautaro <lauti.damore@gmail.com>
2019-08-20 21:46:36 -0300

    pray

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const path = require("path");
 
 require("dotenv").config({ path: '.env' });
 
